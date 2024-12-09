845246bca39ac3de0d032004bf7f1e7326126151
Yugender025 <pentapuriyugender@gmail.com>
2024-12-09 10:28:23 +0530

    properties details

---
+++ assets/js/propertiesdetails.js
@@ -0,0 +1,179 @@
+const propertiesdetails = [
+  {
+    id: 1,
+    locationLink: "",
+    img1: "",
+    img2: "",
+    img3: "",
+    contactlink: "",
+    projectTitle: "",
+    projectDescrp: "",
+    mainHeader: "",
+    mainDescrp: "",
+    mainCaption: "",
+  },
+  {
+    id: 2,
+    locationLink: "",
+    img1: "",
+    img2: "",
+    img3: "",
+    contactlink: "",
+    projectTitle: "",
+    projectDescrp: "",
+    mainHeader: "",
+    mainDescrp: "",
+    mainCaption: "",
+  },
+  {
+    id: 2,
+    locationLink: "",
+    img1: "",
+    img2: "",
+    img3: "",
+    contactlink: "",
+    projectTitle: "",
+    projectDescrp: "",
+    mainHeader: "",
+    mainDescrp: "",
+    mainCaption: "",
+  },
+];
+
+function creatPropertiesdetails(details) {
+  return `
+      <div class="mainProjectcontainer">
+        <div class="locationContainer">
+            <div class="mapContainer">
+                <div id="map-container" style="width: 100%; height: 400px; border-radius: 12px; margin-bottom: 2rem;">
+                    <iframe 
+                    style="width: 100%; height: 100%; border-radius: 12px; border: none;" 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3804.9720356264497!2d78.46000507369244!3d17.508848299297643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb91c420d0b055%3A0xfa2c0b6d5571fc61!2sG%20R%20Mayoora!5e0!3m2!1sen!2sin!4v1733224910037!5m2!1sen!2sin&center=17.508848299297643,78.46500507369244" 
+                    allowfullscreen="" 
+                    loading="lazy" 
+                    referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                
+                </div>
+            </div>
+            <div class="container_info">
+                For sale
+            </div>
+            <div class="projectContainer">
+                <div class="imgsContainer">
+                    <div class="mainImgcontainer">
+                        <img src="https://rightkeyrealty.in/images/mayoora-1.jpg" alt="" srcset="">
+                    </div>
+                    <div class="seeAllcontainer">
+                        <p>See all</p>
+                    </div>
+                </div>
+                <div class="projectInfoContainer">
+                    <div class="infoHeadercontainer">
+                        <h3>GR Mayoora</h3>
+                        <p class="descrp">
+                            Sy.No:117 & 118 of Suchitra & Sy.No:63 of Petbasheerbad village, Near Godavari Homes, Medchal-Malkajgiri(Dist), T.S
+                        </p>
+                    </div>
+                    <div class="secondaryImgs">
+                        <div class="imgsecContainer">
+                            <img src="https://rightkeyrealty.in/images/mayoora-2.jpg" alt="" srcset="">
+                        </div>
+                        <div class="imgsecContainer">
+                            <img src="https://rightkeyrealty.in/images/mayoora-3.jpg" alt="" srcset="">
+                        </div>
+                    </div>
+                    <div class="property-card__cta-button">
+                        <div class="property-card__cta-text">Contact</div>
+                        <div class="property-card__cta-icon"><i class="fa-regular fa-arrow-right"></i></div>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="projectdetails_Container">
+            <div class="projectheader_Container">
+                <div class="main_headerContainer">
+                    <h2>GR MAYOORA Near Godavari Homes, Medchal-Malkajgiri(Dist), T.S</h2>
+                </div>
+                <div class="caption_headerContainer">
+                    <h3 class="highlight">Ready to move in 2&3 BHK Gated Community Apartments </h3>
+                    <p>Luxury Living at GR Mayoora</p>
+                </div>
+            </div>
+            <hr style="margin-top: 0.8rem;">
+            <div class="projectflex_details">
+                <div class="Overview">
+                    Overview
+                </div>
+                <div class="amientiesContainer">
+                    Amienties
+                </div>
+                <div class="Locationhighlights">
+                    Location
+                </div>
+                <div class="aboutprojectContainer">
+                    About
+                </div>
+                <div class="property-card__cta-button">
+                    <div class="property-card__cta-text">Contact</div>
+                    <div class="property-card__cta-icon"><i class="fa-regular fa-arrow-right"></i></div>
+                 </div>
+            </div>
+            <hr>
+            <div class="content_projectDetails">
+                <!-- <content></content> -->
+                <p id="about">Experience the epitome of luxury living with GR Mayoora, where every detail is crafted to elevate your lifestyle
+                    <br>
+                    <br>
+                    Nestled in the tranquil neighborhood of Suchitra, these spacious and splendid 2 and 3-bedroom apartments redefine contemporary living. From the moment you step inside, you'll be greeted by meticulously designed spaces that exude elegance and sophistication. Whether it's unwinding in the lavish interiors, indulging in the state-of-the-art amenities, or simply finding solace in your favorite corner, GR Mayoora offers a sanctuary where you can create cherished memories and embrace the finer things in life. Come, 
+                    embark on a journey of luxury and comfort, and make GR Mayoora your ultimate abode where every moment is a celebration of exquisite living.
+                    <br>
+                    <br>
+                    GR Mayoora, nestled in the highly sought-after location of Suchitra, stands as a testament to the rapid growth and development of the area, 
+                    particularly being adjacent to the prominent landmark of Kompally. This bustling neighborhood boasts proximity to everything you need for a convenient lifestyle – from leading 
+                    tech parks to renowned educational institutions, top-notch healthcare facilities, shopping centers, and entertainment hubs, all within arm's reach. The apartments at GR Mayoora are 
+                    designed to provide residents with a luxurious lifestyle. They offer spacious 2 and 3-bedroom options that redefine contemporary living. 
+                </p>
+            </div>
+            <hr>
+            <div class="projectmanager_contact">
+                <div class="manager_heroContainer">
+                    <img src="assets/images/aravind_hero.jpg" alt="manager" srcset="">
+                </div>
+                <div class="manager_detailsContainer">
+                    <div class="manager_socail_icons">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </div>
+                    <div class="manager_infoMaincontainer">
+                        <div class="manager_info">
+                            <h4>Aravind Reddy</h4>
+                            <p>Sales Manager</p>
+                        </div>
+                        <div class="contacts_container">
+                            <div class="contact_container">
+                                <i class="fa-regular fa-phone"></i>
+                                <a href="tel:+918885554050">8885554050</a>
+                            </div>
+                            <div class="contact_container">
+                                <i class="fa-regular fa-envelope"></i>
+                                <a href="mailto:aravind@digitalwin.in">aravind@digitalwin.in</a>
+                            </div>
+                        </div>
+                       
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    `;
+}
+
+const propertiesDetailscontainer = document.querySelector(
+  ".propertyDetail_info"
+);
+propertiesdetails.forEach((property) => {
+  const propertyCard = creatPropertiesdetails(property);
+  propertiesDetailscontainer.innerHTML += propertyCard;
+});
